4d4007c3655012bb876a0e7487989e731dc32aa7
muratozbk <slipstream11@yandex.com>
2023-09-04 14:12:10 +0300

    parsing url

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const http = require('http');
+const path = require('path');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
+    if (req.url === '/favicon.ico') return;
+
+    console.log(req.headers);
+
+    const myUrl = new URL(req.url, `http://http://${req.headers.host}/`)
+    const pathname = myUrl.pathname;
+    const id = myUrl.searchParams.get('id')
+    console.log(pathname, id);
 
     res.writeHead(200, { 'Content-Type': 'text/html' })
     res.end('<h1>Welcome to the bicycle shop</h1>')
 });
 
 server.listen(3000);
+//npm run server
