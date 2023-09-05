b322a2954da48e235d50cbd911b12438e919b6de
muratozbk <slipstream11@yandex.com>
2023-09-05 17:40:35 +0300

    render dynamic data for overview page

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ const http = require('http');
 const path = require('path');
 const url = require('url');
 const fs = require('fs').promises;
+const bicycles = require('./data/data.json')
 
 const server = http.createServer(async (req, res) => {
     if (req.url === '/favicon.ico') return;
 
-    console.log(req.headers);
-
     const myUrl = new URL(req.url, `http://http://${req.headers.host}/`)
     const pathname = myUrl.pathname;
     const id = myUrl.searchParams.get('id')
-    console.log(req.url);
 
     if (pathname === '/') {
         const html = await fs.readFile('./view/bicycles.html', 'utf-8')
@@ -19,7 +17,19 @@ const server = http.createServer(async (req, res) => {
         res.end(html);
 
     } else if (pathname === '/bicycle' && id >= 0 && id <= 5) {
-        const html = await fs.readFile('./view/overview.html', 'utf-8')
+        let html = await fs.readFile('./view/overview.html', 'utf-8')
+
+        const bicycle = bicycles.find((b) => b.id === id)
+
+        html = html.replace(/<%IMAGE%>/g, bicycle.image)
+        html = html.replace(/<%NAME%>/g, bicycle.name)
+        let price = bicycle.originalPrice;
+        if (bicycle.hasDiscount) {
+            price = (price * (100 - bicycle.discount)) / 100;
+        }
+        html = html.replace(/<%NAMEPRICE%>/g, `$${price}`)
+
+
         res.writeHead(200, { 'Content-Type': 'text/html' })
         res.end(html);
     } else if (/\.(png)$/i.test(req.url)) {
